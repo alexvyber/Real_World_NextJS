6eae069012784f5b9af559cefadb83554f3b7dcd
Alexey Sokolov <alexvyber@gmail.com>
2022-11-09 07:07:23 +0300

    finished 1 chapter

---
+++ my-first-next-app/pages/_app.js
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+
+Math.random() |> (x) => x * 10 |> console.log;
+
+"asdfadfasdf" |> console.log;
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
